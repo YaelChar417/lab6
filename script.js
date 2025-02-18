7615f7bc096efce124bc773901b1cf6b5d831a55
YaelChar417 <A01711111@tec.mx>
2025-02-18 09:49:13 -0600

    Update: botones añadidos para modificar tamaño, color y fuente de un parrafo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const btn = document.getElementById("btn_calcular");
+const btn_calcular = document.getElementById("btn_calcular");
 const select = document.getElementById("producto");
 const img = document.getElementById("imgContainer")
 const subtotal_container = document.getElementById("subtotal");
@@ -33,10 +33,53 @@ const calcularCosto = () => {
             total_container.value = (precio * cantidad) + ((precio * cantidad) * iva);
             img.innerHTML = `<img alt="carne de cerdo" class="card short--img" src="img/cerdo.webp">`
         } else
-        {
+        {   
             img.innerHTML = `<img alt="Selecciona elemento valido" class="card short--img" src="">`
         }
-    
 }
 
-btn.onclick = calcularCosto;
+btn_calcular.onclick = calcularCosto;
+
+const btn_tam = document.getElementById("cambiarTam");
+const btn_fuente = document.getElementById("cambiarTipo");
+const btn_color = document.getElementById("cambiarColor");
+
+const cambiarTamanio = () =>{
+    const text = document.getElementById("cambiar");
+    text.style.fontSize = "2rem";
+    btn_tam.onclick = ponerTamOriginal;
+}
+
+const cambiarFuente = () => {
+    const text = document.getElementById("cambiar");
+    text.style.fontFamily = "Comic Sans";
+    btn_fuente.onclick = ponerFuenteOriginal;
+}
+
+const cambiarColor = () => {
+    const text = document.getElementById("cambiar");
+    text.style.color = "white";
+    btn_color.onclick = ponerColorOriginal;
+}
+
+const ponerTamOriginal = () => {
+    const text = document.getElementById("cambiar");
+    text.style.fontSize = "1rem";
+    btn_tam.onclick = cambiarTamanio;
+}
+
+const ponerFuenteOriginal = () => {
+    const text = document.getElementById("cambiar");
+    text.style.fontFamily = "sans-serif";
+    btn_fuente.onclick = cambiarFuente;
+}
+
+const ponerColorOriginal = () => {
+    const text = document.getElementById("cambiar");
+    text.style.color = "black";
+    btn_color.onclick = cambiarColor;
+}
+
+btn_tam.onclick = cambiarTamanio;
+btn_fuente.onclick = cambiarFuente;
+btn_color.onclick = cambiarColor;
