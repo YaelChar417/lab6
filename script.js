e370d012381dfd095cac02280fa10927f2956bb7
YaelChar417 <A01711111@tec.mx>
2025-02-18 16:50:29 -0600

    Update: Funcionalidades con javascript finalizadas, estructura de preguntas creada

--- script.js
+++ script.js
@@ -80,6 +80,39 @@ const ponerColorOriginal = () => {
     btn_color.onclick = cambiarColor;
 }
 
-btn_tam.onclick = cambiarTamanio;
-btn_fuente.onclick = cambiarFuente;
-btn_color.onclick = cambiarColor;
+btn_tam.onmouseover  = cambiarTamanio;
+btn_fuente.onmouseover = cambiarFuente;
+btn_color.onmouseover = cambiarColor;
+
+
+const info1 = document.getElementById("info1");
+const info2 = document.getElementById("info2");
+
+const ponerInformacionIva = () => {
+    const infoIva = document.getElementById("infoIva");
+    infoIva.innerHTML = `<p>El IVA es del 16% y se calcula multiplicando el subtotal por esta cantidad, si deseas saber más acerca del iva haz click <a target="_blank" href="https://www.bbva.mx/educacion-financiera/impuestos-que-es-el-iva.html">aquí</a></p>`;
+    info1.onclick = quitarInformacionIva;
+
+}
+
+const quitarInformacionIva = () => {
+    const infoIva = document.getElementById("infoIva");
+    infoIva.innerHTML = "";
+    info1.onclick = ponerInformacionIva;
+}
+
+info1.onclick = ponerInformacionIva;
+
+const ponerInformacionTotal = () => {
+    const infoTotal = document.getElementById("infoTotal");
+    infoTotal.innerHTML = `<p>El total se calcula sumando el subtotal más el iva, no se aplican cargos por envio y solo enviamos a México, para verificar más visite el siguiente enlace <a target="_blank" href="https://www.google.com.mx/maps/preview">aquí</a></p>`;
+    info2.onclick = quitarInformacionTotal;
+}
+
+const quitarInformacionTotal = () => {
+    const infoTotal = document.getElementById("infoTotal");
+    infoTotal.innerHTML = "";
+    info2.onclick = ponerInformacionTotal;
+}
+
+info2.onclick = ponerInformacionTotal;
